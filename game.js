41a5cd06cf78372363be60ce9eefb09aa0a9f3bd
Paweł Nieczuja-Ostrowski <pawel.nieczuja@onet.eu>
2020-11-21 00:53:24 +0100

    Fix chances

--- game.js
+++ game.js
@@ -1,107 +1,118 @@
 //zmienne stanu: np. lista słów, funckja losująca, kiedy zakończyć grę, 
 
-(function () {
+const gameHangman = () => {
 
-	//DOM elems
-	const game = document.getElementById('game');
-	const gameTitle = game.getElementsByClassName('game-name')[0];
-	const wordContainer = game.getElementsByClassName('hidden-word')[0];
-	const chancesText = game.getElementsByClassName('chances')[0];
-	const chancesNmbr = game.getElementsByClassName('chances-number')[0];
-	const letterContainer = game.getElementsByClassName('letter-container')[0];
-	const button = game.getElementsByClassName('button')[0];
-	const resultText = game.getElementsByClassName('result')[0];
+  //DOM elems
+  const game = document.getElementById('game');
+  const name = document.getElementById('name');
+  const restart = document.getElementById('restart');
+  const gameTitle = name.getElementsByClassName('game-name')[0];
+  const wordContainer = game.getElementsByClassName('hidden-word')[0];
+  const chancesText = game.getElementsByClassName('chances')[0];
+  const chancesNmbr = game.getElementsByClassName('chances-number')[0];
+  const letterContainer = game.getElementsByClassName('letter-container')[0];
+  const button = game.getElementsByClassName('button')[0];
+  const resultText = game.getElementsByClassName('result')[0];
+  const buttonRestart = restart.getElementsByClassName('button-restart')[0];
 
+  gameTitle.textContent = 'Szubienica';
+  //lista słów
+  const listOfWords = ['halabardnik', 'hydraulik', 'podpułkownik', 'kamerdyner', 'transporter'];
 
-	gameTitle.textContent = 'Szubienica';
-	//lista słów
-	const listOfWords = ['halabardnik', 'hydraulik', 'podpułkownik', 'kamerdyner'];
+  //losowanie słowa
+  const wordsIndex = Math.floor(Math.random() * listOfWords.length);
+  const word = listOfWords[wordsIndex];
+  let listOfErrors = Math.ceil(word.length / 2);
+  //console.log(listOfErrors)
 
-	//losowanie słowa
-	const wordsIndex = Math.floor(Math.random() * listOfWords.length);
-	const word = listOfWords[wordsIndex];
-	let listOfErrors = 5;
+  //ukrycie słowa
+  const choosenWordArr = Array.from(word);
+  const hiddenWord = Array.from(word, () => '_');
 
-	//ukrycie słowa
-	const choosenWordArr = Array.from(word);
-	const hiddenWord = Array.from(word, () => '_');
+  //console.log(writedLetter);
+  wordContainer.textContent = hiddenWord.join(' ');
 
-	//console.log(writedLetter);
-	wordContainer.textContent = 'Słowo: ' + hiddenWord.join(' ');
+  let arr = [];
+  //komunikat słowo
+  //alert('Ukryte słowo: ' + hiddenWord.join(' ')); //join łączy elementy arraya
 
-	let arr = [];
-	//komunikat słowo
-	//alert('Ukryte słowo: ' + hiddenWord.join(' ')); //join łączy elementy arraya
+  let nmbrOfChances = listOfErrors;
 
-	let nmbrOfChances = listOfErrors;
+  //funkcja dodawania liter
+  const checkLetter = () => {
+    let writedLetter = letterContainer.value;
 
-	//funkcja dodawania liter
-	const checkLetter = () => {
-		let writedLetter = letterContainer.value;
+    let find = choosenWordArr.includes(writedLetter);
 
-		let find = choosenWordArr.includes(writedLetter);
+    //funkcja filtrująca
+    function letterSearch(element, index) {
+      let nmbr = element === writedLetter && index;
+      //console.log(nmbr);
+      if (nmbr !== false) {
+        return nmbr;
+      }
+    }
 
-		//funkcja filtrująca
-		function letterSearch(element, index) {
-			let nmbr = element === writedLetter && index;
-			//console.log(nmbr);
-			if (nmbr !== false) {
-				return nmbr;
-			}
-		}
+    function addLetters(item, index) {
+      if (item !== undefined) {
+        arr[index] = item;
+      }
+    }
 
-		function addLetters(item, index) {
-			if (item !== undefined) {
-				arr[index] = item;
-			}
-		}
+    if (find === true) {
+      //console.log('Trafione');
+      let nmbrArr = choosenWordArr.map(letterSearch);
+      //tu odsłanianie
+      nmbrArr.map(addLetters);
+      //console.log(arr);
+      showingLetters();
 
-		if (find === true) {
-			//console.log('Trafione');
-			let nmbrArr = choosenWordArr.map(letterSearch);
-			//tu odsłanianie
-			nmbrArr.map(addLetters);
-			console.log(arr);
-			showingLetters();
+    } else {
+      //console.log('Nietrafione');
+      //console.log(nmbrOfChances);
+      if (nmbrOfChances > 1) {
+        let chances = nmbrOfChances;
+        nmbrOfChances -= 1;
+        chancesNmbr.textContent = nmbrOfChances;
+      } else {
+        button.removeEventListener('click', checkLetter);
+        chancesNmbr.textContent = 0;
+        resultText.style.color = 'red';
+        resultText.textContent = 'Przegrałeś!';
+      }
+    }
+  }
 
-		} else {
-			//console.log('Nietrafione');
-			//console.log(nmbrOfChances);
-			if (nmbrOfChances > 0) {
-				nmbrOfChances -= 1;
-				chancesNmbr.textContent = nmbrOfChances;
-			} else {
-				button.removeEventListener('click', checkLetter);
-				resultText.style.color = 'red';
-				resultText.textContent = 'Przegrałeś. Spróbuj jeszcze raz.';
-			}
-		}
-	}
+  let word2;
+  chancesNmbr.textContent = nmbrOfChances;
 
-	let word2;
-	chancesNmbr.textContent = nmbrOfChances;
+  //komunikat z pobraniem
+  function showingLetters() {
 
-	//komunikat z pobraniem
-	function showingLetters() {
+    const unHiddenWord = Array.from(word, (item, index) =>
+      index === arr[index] ? item : '_'
+    );
 
-		const unHiddenWord = Array.from(word, (item, index) =>
-			index === arr[index] ? item : '_'
-		);
+    console.log(arr);
 
-		console.log(arr);
+    //sprawdzqnie wartości....
+    wordContainer.textContent = unHiddenWord.join(' ');
+    console.log(choosenWordArr, unHiddenWord)
+    word2 = unHiddenWord.join('');
 
-		//sprawdzqnie wartości....
-		wordContainer.textContent = 'Słowo: ' + unHiddenWord.join(' ');
-		console.log(choosenWordArr, unHiddenWord)
-		word2 = unHiddenWord.join('');
+    if (word2 === word) {
+      resultText.style.color = 'orange';
+      resultText.textContent = 'Cudownie. Wygrałeś!';
+      button.removeEventListener('click', checkLetter);
+    }
+  }
 
-		if (word2 === word) {
-			resultText.style.color = 'orange';
-			resultText.textContent = 'Cudownie. Wygrałeś!';
-			button.removeEventListener('click', checkLetter);
-		}
-	}
+  button.addEventListener('click', checkLetter);
+  buttonRestart.addEventListener('click', () => {
+    gameHangman();
+    resultText.textContent = '';
 
-	button.addEventListener('click', checkLetter);
+  })
+}
 
-}()) //funckja samowywołująca się
+gameHangman()
